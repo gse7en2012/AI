4b8b550a56410ffc87cb9b4fbffbfaa4e6b76528
Gseven <gse7en2010@163.com>
2016-11-21 23:46:34 +0800

    page2

--- js/index.js
+++ js/index.js
@@ -14,6 +14,58 @@
         pageName:['开始','种植站','生产站','包装站','商店'],
         init: function () {
             this.setSwiper();
+            this.setCloseTrigger();
+            this.storyOpenTrigger();
+        },
+        setCloseTrigger:function(){
+            $('.close-btn').on('click', function(event) {
+                var bw = $('.intro-modal').width();
+                $(this).closest('.intro-modal').animate({
+                    right: -bw
+                }, 300);
+                $(this).closest('.intro-modal').removeClass('open').addClass('close');
+            });
+        },
+        storyOpenTrigger:function(){
+            var _this=this;
+            $('.story').on('click', function(event) {
+                var index = Number($(this).attr('index'));
+                $('.intro-main').html(_this.getIntro(index));
+                $('.intro-modal').animate({
+                    right: 0
+                }, 300);
+                $('.intro-modal').removeClass('close').addClass('open');
+            });
+        },
+        storyList:[
+            {
+                name:'杨海林',
+                title:'提取部提取岗位员工',
+                age:'工作年限9年',
+                word:'“在高温之下，始终坚持将每一个流程进行人工复核<br>只为了极致的标准。”',
+                img:'images/page3/yhl.jpg',
+                content:'100%的极致<br>杨海林每天的任务，除了在总控制室操作提取浓缩的各个工序，还要在高温的提取浓缩部进行检核的工作。无限极引进国内领先的提取浓缩自动化技术，增健药液的浓缩指标为1300公斤，当达到指标时机器会自动监测停止。尽管已实现机器自动化，但为了精益求精，提取、浓缩、冷却、灌装，每一个流程，杨海林还会进行人工复核，查看是否达到真正这个标准。'
+            },
+            {
+                name:'潘守俊',
+                title:'口服液现场质检工程师',
+                age:'工作年限10年',
+                word:'“质检是至关重要的质量把控环节，<br>因此我所负责的每一项检验都要‘锱铢必较’”',
+                img:'images/page4/psj.jpg',
+                content:'100％的严谨<br>潘守俊对产品质量的严格要求，体现在其对生产线的各项指标的精确检验，十年来始终如一。从生产前产品所有原材料的质量是否符合标准，生产中配置的液料取样是否达标，到成品的相对密度、固体物、微生物、PH值、多糖含量等各项数值指标，潘守俊的质检工作都一步不落，严谨而有序地进行，每一项检测都严格把控，即使只是一丝一毫的误差，都绝不放过。'
+            }
+        ],
+        getIntro(index){
+            var person=this.storyList[index];
+            var html='<div>';
+            html+='<div class="name">'+person.name+'</div>';
+            html+='<div class="title">'+person.title+'</div>';
+            html+='<div class="age">'+person.age+'</div>';
+            html+='<img src="'+person.img+'" class="avatar">';
+            html+='<div class="word">'+person.word+'</div>';
+            html+='<div class="content">'+person.content+'</div>';
+            html+='</div>';
+            return html;
         },
         setSwiper: function () {
             var winSize=this.getWinSize();
@@ -46,8 +98,10 @@
                 onTransitionEnd:function(sw){
                    if(sw.activeIndex==4){
                        $('.pc5-tip').addClass('active');
+                       $('.pc5-guide').addClass('active');
                    }else{
                        $('.pc5-tip').removeClass('active');
+                       $('.pc5-guide').removeClass('active');
                    }
                 }
             });
